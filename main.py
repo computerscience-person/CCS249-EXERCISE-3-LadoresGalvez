6729b13fa2fdd85c1aaf20693e09eaa039f1ca0a
usernameiskheejay <galvez.kjm@optimixr.com>
2025-03-08 15:14:55 +0800

    generate bigram and trigram models

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import wikipedia as wk
 import nltk
+from collections import Counter
+from nltk.util import bigrams, trigrams
 
 def main():
     # Retrieve Wikipedia article
@@ -12,5 +14,24 @@ def main():
     # Limit to 1000 words
     article_toks = article_toks[:1000]
 
+    # Generate bigram and trigram models
+    def bigram_probabilities(tokens):
+        bigram_counts = Counter(bigrams(tokens))
+        unigram_counts = Counter(tokens)
+
+        bigram_probs = {bigram: count / unigram_counts[bigram[0]]
+                        for bigram, count in bigram_counts.items()}
+        return bigram_probs
+
+    def trigram_probabilities(tokens):
+        trigram_counts = Counter(trigrams(tokens))
+        bigram_counts = Counter(bigrams(tokens))
+
+        trigram_probs = {trigram: count / bigram_counts[(trigram[0], trigram[1])]
+                        for trigram, count in trigram_counts.items() if (trigram[0], trigram[1]) in bigram_counts}
+        return trigram_probs
+
+    # print(bigram_probabilities(article_toks))
+    print(trigram_probabilities(article_toks))
 if __name__ == "__main__":
     main()
