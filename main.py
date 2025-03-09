f4695a2ede07ccaf4fc77e0cecc555e5bcc6038a
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2025-03-09 20:12:29 +0800

    Add some typing and refactored the functions.

--- main.py
+++ main.py
@@ -1,42 +1,48 @@
+from typing import cast
+import asyncio
 import wikipedia as wk
 import nltk
 from collections import Counter
 from nltk.util import bigrams, trigrams
+from pprint import pprint
 
-def main():
+async def main():
+    await create_and_run_models()
+
+async def create_and_run_models():
     # Retrieve Wikipedia article
     article = wk.page("Python (programming language)")
-    tokzr = nltk.tokenize.RegexpTokenizer(r'\b(?:\w+(?:\.\w+)*|\d+\.\d+)\b')
-    article_toks = tokzr.tokenize(article.content.lower())
-    print(len(article_toks))
+    # Use regular expression tokenizer
+    tokzr = nltk.tokenize.RegexpTokenizer(r"\b(?:\w+(?:\.\w+)*|\d+\.\d+)\b")
+    article_toks = cast(list[str], tokzr.tokenize(article.content.lower()))
     # Limit to 1000 words
     article_toks = article_toks[:1000]
-    print(*(tok for tok in article_toks), sep = " ")
-
     # Generate bigram and trigram models
-    def bigram_probabilities(tokens):
-        bigram_counts = Counter(bigrams(tokens))
-        unigram_counts = Counter(tokens)
+    pprint(bigram_probabilities(article_toks))
+    pprint(trigram_probabilities(article_toks))
 
-        bigram_probs = {bigram: count / unigram_counts[bigram[0]]
-                        for bigram, count in bigram_counts.items()}
-        return bigram_probs
+def bigram_probabilities(tokens: list[str]) -> dict[tuple[str, str], float]:
+    bigram_counts = Counter(bigrams(tokens))
+    unigram_counts = Counter(tokens)
 
-    def trigram_probabilities(tokens):
-        trigram_counts = Counter(trigrams(tokens))
-        bigram_counts = Counter(bigrams(tokens))
+    bigram_probs = {
+        bigram: count / unigram_counts[bigram[0]]
+        for bigram, count in bigram_counts.items()
+    }
+    return bigram_probs
 
-        trigram_probs = {trigram: count / bigram_counts[(trigram[0], trigram[1])]
-                        for trigram, count in trigram_counts.items() if (trigram[0], trigram[1]) in bigram_counts}
-        return trigram_probs
 
-    # print(bigram_probabilities(article_toks))
-    print(trigram_probabilities(article_toks))
-def bigrams_model():
-    ...
+def trigram_probabilities(tokens: list[str]) -> dict[tuple[str, str, str,], float]:
+    trigram_counts = Counter(trigrams(tokens))
+    bigram_counts = Counter(bigrams(tokens))
 
-def trigram_model():
-    ...
+    trigram_probs = {
+        trigram: count / bigram_counts[(trigram[0], trigram[1])]
+        for trigram, count in trigram_counts.items()
+        if (trigram[0], trigram[1]) in bigram_counts
+    }
+    return trigram_probs
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
